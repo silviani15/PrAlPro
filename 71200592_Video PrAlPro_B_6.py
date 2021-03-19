ebe6c0b9e04ff553aca6249b9beb4a69181c14c5
silviani15 <78337142+silviani15@users.noreply.github.com>
2021-03-19 21:18:22 +0700

    Add files via upload

---
+++ 71200592_Video PrAlPro_B_6.py
@@ -0,0 +1,22 @@
+# Silviani Vanesha / 71200592
+# Universitas Kristen Duta Wacana
+# Informatika / 20
+# String
+
+'''
+Input : 
+    1. User memasukkan kalimat yang akan dihitung
+Proses : 
+    1. Mencari Jumlah kata
+    2. Mencari Jumlah Karakter
+Output : 
+    1. Menghasilkan jawaban yang dicari yaitu menentukan jumlah kata dan jumlah karakter
+'''
+
+inp = str(input("Masukkan Kalimat : "))
+def penghitungan_kata():
+    hitung_karakter = inp.split()
+    print("Jumlah Karakter : ",len(inp))
+    hitung_kata = len(hitung_karakter)
+    print("Jumlah Kata : ",hitung_kata)
+penghitungan_kata()
